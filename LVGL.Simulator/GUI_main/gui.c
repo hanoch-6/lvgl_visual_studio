3303354f3fe75fc6bb5596b2d5dd75bdc69edd08
hanoch <hanoch1024@fomxail.com>
2022-05-31 00:14:35 +0800

    自动定时器框架

--- LVGL.Simulator/GUI_main/gui.c
+++ LVGL.Simulator/GUI_main/gui.c
@@ -30,7 +30,10 @@ static void screen_clean_up(void* scr);
 /**********************
 *  STATIC VARIABLES
 **********************/
-lv_obj_t* start_ui;
+static lv_obj_t* start_anim_obj;
+static lv_obj_t* menu_obj;
+
+lv_obj_t* backup;
 /**********************
 *      MACROS
 **********************/
@@ -41,9 +44,9 @@ lv_obj_t* start_ui;
 
 void gui_start(void)
 {
-    lv_obj_t* backup = start_anim_create(lv_scr_act());
+    start_anim_obj = lv_scr_act();
+    menu_obj = lv_scr_act();
     lv_timer_t* timer = lv_timer_create(auto_step_cb, 1000, NULL);
-    lv_timer_set_repeat_count(timer, 5);
 }
 /**********************
 *   STATIC FUNCTIONS
@@ -52,30 +55,38 @@ static lv_obj_t* start_anim_create(lv_obj_t* parent)
 {
     LV_IMG_DECLARE(xiaoxin);
 
+    printf("start anim create\n");
     lv_obj_t* cont= lv_obj_create(parent);
-    lv_obj_set_height(cont, LV_SIZE_CONTENT);
-    lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_flex_align(cont, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    //TODO 设置上下滑动
+    //lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
+
+    lv_obj_set_size(cont, LV_HOR_RES, LV_VER_RES);
+    lv_obj_set_align(cont, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
     lv_obj_t* gif_xiaoxin = lv_gif_create(cont);
     lv_gif_set_src(gif_xiaoxin, &xiaoxin);
-    lv_obj_align(gif_xiaoxin, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(gif_xiaoxin, LV_ALIGN_CENTER, 0, -HOR_STEP);
 
     lv_obj_t* scroll_Info = lv_label_create(cont);
     lv_label_set_long_mode(scroll_Info, LV_LABEL_LONG_SCROLL_CIRCULAR);
-    lv_obj_set_width(scroll_Info, 180);
-    lv_label_set_text(scroll_Info, "Hanoch&小新, 三年2班\nLVGL version:8.3");
+    lv_obj_set_width(scroll_Info, 150);
+    lv_label_set_text(scroll_Info, "黄妙之&小新, 三年2班\nLVGL version:8.3");
     lv_obj_align(scroll_Info, LV_ALIGN_BOTTOM_MID, 0, -HOR_STEP);
     
     return cont;
 }
 static lv_obj_t* menu_create(lv_obj_t* parent)
 {
-    LV_IMG_DECLARE(sunny);
+    LV_IMG_DECLARE(weather);
     printf("menu create\n");
-    lv_obj_t* weather_obj = lv_img_create(lv_scr_act());
-    lv_img_set_src(weather_obj, &sunny);
+    lv_obj_t* weather_obj = lv_gif_create(parent);
+    lv_obj_set_height(weather_obj, LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(weather_obj, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_align(weather_obj, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER);
+    lv_gif_set_src(weather_obj, &weather);
     lv_obj_align(weather_obj, LV_ALIGN_CENTER, 0, 0);
+
+    return weather_obj;
 }
 static void auto_step_cb(lv_timer_t* timer)
 {
@@ -84,19 +95,20 @@ static void auto_step_cb(lv_timer_t* timer)
 
     switch (state)
     {
-        case 5:
+        case 0:
         {
-
+            backup = start_anim_create(start_anim_obj);
         }
         break;
-        case 6:
+        case 5:
         {
-
+            backup = menu_create(start_anim_obj);
         }
         break;
         default :
         break;
     }
+    state++;
 }
 static void screen_clean_up(void* scr)
 {
